bcd18be5e871c229ee9c8c706a324e4d948cb25c
Rehmat <rehmat@5683.me>
2022-12-29 14:32:34 +0530

    Updated

---
+++ utils/email.js
@@ -0,0 +1,9 @@
+exports = module.exports = exports = module.exports = function() {
+  var mod = {
+    valid: function(email) {
+      var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(email).toLowerCase());
+    }
+  };
+  return mod;
+};
